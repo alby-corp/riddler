7fed9f7d8acc01a86072f5bbea62f8802f60bc3c
Andrea Candeliere <candelierea@ynap.world>
2023-10-30 14:47:17 +0100

    Implement the rich model using records

--- src/Assessment.Console/Models/Csv.cs
+++ src/Assessment.Console/Models/Csv.cs
@@ -1,13 +1,3 @@
 namespace Assessment.Console.Models;
 
-public class Csv
-{
-    public string? GivenName { get; }
-    public string? FamilyName { get; }
-
-    public Csv(string givenName, string familyName)
-    {
-        GivenName = givenName;
-        FamilyName = familyName;
-    }
-}
+public record Csv(string? GivenName, string? FamilyName);
